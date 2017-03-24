5731b39081bfb9f0da2328c504058ad8011fa2ab
deshanigtk <deshani.14@cse.mrt.ac.lk>
2017-03-24 17:59:14 +0530

    adds a map still not working

--- client/main.js
+++ client/main.js
@@ -17,14 +17,4 @@ Template.master_layout.onRendered(function () {
 //     });
 // });
 
-Template.register_layout.onRendered(function () {
-    $.getScript("plugins/jQuery/jquery-2.2.3.min.js");
-    $.getScript("bootstrap/js/bootstrap.min.js");
-    $.getScript("plugins/iCheck/icheck.min.js", function () {
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue',
-            increaseArea: '20%' // optional
-        });
-    });
-});
+
