9df0e0d1cf1b3aec29bec3ed31a2609019d51dde
deshanigtk <deshani.14@cse.mrt.ac.lk>
2017-03-26 07:26:36 +0530

    radio button input

--- client/user_admin/scripts/register_driver.js
+++ client/user_admin/scripts/register_driver.js
@@ -7,13 +7,14 @@ Template.register_driver.events({
        const driverLastName=event.target.driverLastName.value;
        const driverContactNo=event.target.driverContactNo.value;
        const driverEmail=event.target.driverEmail.value;
+       const vehicleType = "Van";
 
-       if(event.target.optionsRadios1.checked) {
-           const vehicleType = "Van";
-       }
-       else{
-           const vehicleType = "Bus";
-       }
+       // if(event.target.optionsRadios1.checked) {
+       //     const vehicleType = "Van";
+       // }
+       // else{
+       //     const vehicleType = "Bus";
+       // }
 
        const seatCount=event.target.seatCount.value;
        const plateNo=event.target.plateNo.value;
