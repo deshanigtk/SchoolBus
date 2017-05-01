fb9061dbe9688cd809af732685ae79777716be7f
deshanigtk <deshani.14@cse.mrt.ac.lk>
2017-05-01 07:16:29 +0530

    add a map2

---
+++ client/common/scripts/driver_profile.js
@@ -0,0 +1,3 @@
+Template.driver_profile.onRendered(function() {
+    GoogleMaps.load({key:'AIzaSyDz4OqXSPfYD-AvW1SopPLMBoW9n1c90Qg'});
+});
--- client/partials/scripts/map.js
+++ client/partials/scripts/map.js
@@ -1,5 +1,5 @@
 Template.map.helpers({
-    exampleMapOptions: function() {
+    exampleMapOptions: function () {
         // Make sure the maps API has loaded
         if (GoogleMaps.loaded()) {
             // Map initialization options
@@ -10,26 +10,70 @@ Template.map.helpers({
         }
     }
 });
+GoogleMaps.ready('exampleMap', function (map) {
+    // map.instance, map.options
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 6.927079, lng: 79.861244},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
 
-// Template.map.onCreated(function() {
-//     // We can use the `ready` callback to interact with the map API once the map is ready.
-//     GoogleMaps.ready('exampleMap', function(map) {
-//         // Add a marker to the map once it's ready
-//         var marker = new google.maps.Marker({
-//             position: map.options.center,
-//             map: map.instance
-//         });
-//     });
-// });
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-Template.map.onRendered(function() {
-    GoogleMaps.load({key:'AIzaSyDz4OqXSPfYD-AvW1SopPLMBoW9n1c90Qg'});
-});
-GoogleMaps.create({
-    name: 'exampleMap',
-    element: document.getElementById('exampleMap'),
-    options: {
-        center: new google.maps.LatLng(6.927079, 79.861244),
-        zoom: 8
-    }
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
 });
