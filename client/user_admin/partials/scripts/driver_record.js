cb6bed3cb01d604873eb809aff1bfe20759ccf3c
deshanigtk <deshani.14@cse.mrt.ac.lk>
2017-05-16 12:44:48 +0530

    few changes

--- client/user_admin/partials/scripts/driver_record.js
+++ client/user_admin/partials/scripts/driver_record.js
@@ -7,16 +7,16 @@ Template.driver_record.helpers({
     }
 });
 
-Template.driver_record.events({
-    'click #single_driver_record': function (event) {
-        event.preventDefault();
-
-        BlazeLayout.render('master_layout', {
-            content: 'edit_driver',
-            side_bar_links: 'admin_side_bar_links',
-            user_data: {
-                user_id: Template.instance().data._id,
-            }
-        });
-    }
-});
+// Template.driver_record.events({
+//     'click #single_driver_record': function (event) {
+//         event.preventDefault();
+//
+//         BlazeLayout.render('master_layout', {
+//             content: 'edit_driver',
+//             side_bar_links: 'admin_side_bar_links',
+//             user_data: {
+//                 user_id: Template.instance().data._id,
+//             }
+//         });
+//     }
+// });
